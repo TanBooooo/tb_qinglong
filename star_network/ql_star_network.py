06dd42bc9649fc03bb3a68a6b84f14018de7969e
TanBooooo <18580902653@163.com>
2022-12-06 22:21:21 +0800

    star令牌签到

--- star_network/ql_star_network.py
+++ star_network/ql_star_network.py
@@ -23,15 +23,18 @@ logging.basicConfig(
     datefmt="%H:%M:%S"
 )
 logger = logging.getLogger(application_name)
-success_count = 0
-error_count = 0
-error_email = []
+
+total_count = 0
+login_fail_msg = []
+start_fail_msg = []
+sign_fail_msg = []
+signing_msg = []
 
 
 def encrypt(payload):
     timestamp = int(time.time() * 1000)
-    sign = hashlib.md5(
-        (str(payload) + ':D7C92C3FDB52D54147B668DC6F8A5@' + str(timestamp)).replace("'", '"').encode()).hexdigest()
+    data = (str(payload) + ':D7C92C3FDB52D54147B668DC6F8A5@' + str(timestamp)).replace("'", '"').encode()
+    sign = hashlib.md5(data).hexdigest()
     payload['timestamp'] = timestamp
     payload['hash'] = sign
     return payload
@@ -42,73 +45,68 @@ class StarNetwork(object):
         self.email = email
         self.password = password
         self.headers = {"User-Agent": "Dart/2.16 (dart:io)"}
+        self.message = ''
         self.error_msg = ''
-        self.success = False
 
     def login(self):
         payload = {"email": self.email, "password": self.password}
-        res = requests.post("https://api.starnetwork.io/v1/email/login", json=encrypt(payload))
-        if res.text.count('jwt') > 0:
-            token = res.json()['jwt']
-            self.headers['Authorization'] = "Bearer {}".format(token)
-            logger.info('登录成功----{}'.format(self.email))
-        else:
-            self.error_msg = '登录失败----{}----{}'.format(self.email, res.text)
-            logger.error(self.error_msg)
-
-        return res.text.count('jwt') > 0
+        try:
+            res = requests.post("https://api.starnetwork.io/v1/email/login", json=encrypt(payload))
+            if res.text.count('jwt') > 0:
+                token = res.json()['jwt']
+                self.headers['Authorization'] = "Bearer {}".format(token)
+                logger.info("登录成功----{}".format(self.email))
+            else:
+                login_fail_msg.append('登录失败----{}----{}'.format(self.email, res.text))
+            return res.text.count('jwt') > 0
+        except Exception as ex:
+            login_fail_msg.append('登录失败----{}----{}'.format(self.email, ex))
 
     def start(self):
-        res = requests.post("https://api.starnetwork.io/v1/session/start", headers=self.headers)
-        if res.text.count('endAt') > 0:
-            logger.info("挖矿成功----{}".format(self.email))
-            self.success = True
-        else:
-            self.error_msg = '挖矿失败----{}----{}'.format(self.email, res.text)
-            logger.error(self.error_msg)
-        return res.text.count('endAt') > 0
+        try:
+            res = requests.post("https://api.starnetwork.io/v1/session/start", headers=self.headers)
+            if res.text.count('endAt') > 0:
+                logger.info("挖矿成功----{}".format(self.email))
+            else:
+                start_fail_msg.append('挖矿失败----{}----{}'.format(self.email, res.text))
+            return res.text.count('endAt') > 0
+        except Exception as ex:
+            start_fail_msg.append('挖矿失败----{}----{}'.format(self.email, ex))
+
+    def sign(self):
+        try:
+            res = requests.post('https://api.starnetwork.io/v3/user/checkin', headers=self.headers)
+            if res.text.count('CLAIMED') > 0:
+                logger.info("签到成功----{}".format(self.email))
+            else:
+                res = requests.get('https://api.starnetwork.io/v3/user/checkin', headers=self.headers)
+                if res.json()['checkins'][0] is not None and res.json()['checkins'][0] != '':
+                    signing_msg.append("未到签到时间----{}".format(self.email))
+                else:
+                    sign_fail_msg.append("签到失败----{}----{}".format(self.email, res.text))
+        except Exception as ex:
+            sign_fail_msg.append("签到失败----{}----{}".format(self.email, ex))
 
     def draw(self):
         try:
-            res = requests.post('https://api.starnetwork.io/v3/user/checkin', headers=self.headers)
-            # logger.error(res.text)
-        except Exception as e:
-            logger.error("{}----令牌签到出错----{}".format(self.email, e))
-        try:
             res = requests.get('https://api.starnetwork.io/v3/libra/draw', headers=self.headers)
-            # logger.error(res.text)
-        except Exception as e:
-            logger.error("{}----加速抽奖出错----{}".format(self.email, e))
+        except Exception as ex:
+            logger.error("{}----加速抽奖出错----{}".format(self.email, ex))
         try:
             res = requests.get('https://api.starnetwork.io/v3/auth/user', headers=self.headers)
-            # logger.error(res.text)
             if res.text.count('{"id":') > 0:
                 payload = {"id": res.json()['id'], "action": "draw_boost"}
                 res = requests.post('https://api.starnetwork.io/v3/event/draw', json=encrypt(payload),
                                     headers=self.headers)
-                # logger.error(res.text)
-        except Exception as e:
-            logger.error("{}----令牌抽奖出错----{}".format(self.email, e))
+        except Exception as ex:
+            logger.error("{}----令牌抽奖出错----{}".format(self.email, ex))
 
 
-def task(star, lock):
-    try:
-        if star.login():
-            star.start()
-            star.draw()
-    except Exception as e:
-        logger.error('{}---出现错误----{}'.format(star.email, e))
-
-    lock.acquire()
-    if star.success:
-        global success_count
-        success_count = success_count + 1
-    else:
-        global error_count
-        global error_email
-        error_count = error_count + 1
-        error_email.append(star.error_msg)
-    lock.release()
+def task(star_network):
+    if star_network.login():
+        star_network.start()
+        star_network.sign()
+        star_network.draw()
 
 
 if __name__ == '__main__':
@@ -119,7 +117,6 @@ if __name__ == '__main__':
     if thread_num < 1:
         thread_num = 1
 
-    lock = threading.RLock()
     pool = ThreadPoolExecutor(max_workers=thread_num)
     for line in lines:
         line = line.strip()
@@ -128,18 +125,33 @@ if __name__ == '__main__':
             email = split[0]
             password = split[1]
             star = StarNetwork(email, password)
-            thread = pool.submit(task, star, lock)
-        except Exception:
-            error_count = error_count + 1
-            error_email.append("数据:{}，账号密码读取失败".format(line))
+            thread = pool.submit(task, star)
+        except Exception as e:
+            login_fail_msg.append("数据: {}，账号密码读取失败，错误: {}".format(line, e))
     pool.shutdown()
 
-    if len(error_email) > 0:
-        logger.error("失败统计: ")
-        for em in error_email:
-            logger.error(em)
+    if len(login_fail_msg) > 0:
+        logger.info("登录失败：")
+        for msg in login_fail_msg:
+            logger.error(msg)
 
-    # logger.info('总计成功数: {}'.format(success_count))
-    # logger.info('总计失败数: {}'.format(error_count))
+    if len(start_fail_msg) > 0:
+        logger.info("挖矿失败：")
+        for msg in start_fail_msg:
+            logger.error(msg)
 
-    send(application_name, '总计成功数: {}\n总计失败数: {}'.format(success_count, error_count))
+    if len(sign_fail_msg) > 0:
+        logger.info("签到失败：")
+        for msg in sign_fail_msg:
+            logger.error(msg)
+
+    if len(signing_msg) > 0:
+        logger.info("未到签到时间：")
+        for msg in signing_msg:
+            logger.error(msg)
+
+    send(application_name, '账号总数: {}\n登录失败数: {}\n挖矿失败数: {}\n签到失败数: {}\n未到签到时间数: {}'.format(len(lines),
+                                                                                           len(login_fail_msg),
+                                                                                           len(start_fail_msg),
+                                                                                           len(sign_fail_msg),
+                                                                                           len(signing_msg)))
